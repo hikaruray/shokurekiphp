783f1b6b6fd5e32c6d350bb48f3bb1d2e07eec81
yasshi <ynishihira@gmail.com>
2022-01-31 14:32:14 +0900

    changed title and meta

--- mail.php
+++ mail.php
@@ -12,11 +12,11 @@ $mess = $_POST['message'];
 <head>
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <title>Profile &mdash; Free Website Template, Free HTML5 Template by freehtml5.co</title>
+    <title>Message Sent</title>
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <meta name="description" content="Free HTML5 Website Template by freehtml5.co" />
-    <meta name="keywords" content="free website templates, free html5, free template, free bootstrap, free website template, html5, css3, mobile first, responsive" />
-    <meta name="author" content="freehtml5.co" />
+    <meta name="description" content="your message has been sent to the owner" />
+    <meta name="keywords" content="web services, customize wordpress, wordpress to html, php system" />
+    <meta name="author" content="https://www.iggygroup.com" />
 
     <link href="https://fonts.googleapis.com/css?family=Space+Mono" rel="stylesheet">
     <link href="https://fonts.googleapis.com/css?family=Kaushan+Script" rel="stylesheet">
